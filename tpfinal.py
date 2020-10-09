ce9091d8bdf8b6a063e9a17fb0cce9f471e242ba
matiaslopezs <matigls2@gmail.com>
2020-10-09 15:02:43 -0300

    Se añade el conjunto consecuente

--- tpfinal.py
+++ tpfinal.py
@@ -26,4 +26,23 @@ def tiempo(x, subconjunto):
     return 0
 
 
+def nivel_pizza(x, subconjunto):
+    if subconjunto == 'poco cocinada':
+        if 0 <= x < 2:
+            return (x - 0) / (2 - 0)
+        elif 2 <= x < 4:
+            return (4 - x) / (4 - 2)
+    elif subconjunto == 'en su punto':
+        if 3 <= x < 5:
+            return (x - 3) / (5 - 3)
+        elif 5 <= x < 7:
+            return (7 - x) / (7 - 5)
+    elif subconjunto == 'muy cocinada':
+        if 6 <= x < 8:
+            return (x - 6) / (8 - 6)
+        elif 8 <= x < 10:
+            return (10 - x) / (10 - 8)
+    return 0
+
+
 print(tiempo(8.5, 'mucho'))
