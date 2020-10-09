bee2b5ce80e9761f89779b635b630f035e52d5b1
luis <dmc.luisalfredo@gmail.com>
2020-10-09 18:29:11 -0300

    Vista básica
    Se agregó una vista en HTML

--- pizzeria/views.py
+++ pizzeria/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainView(request):
+    return render(request, 'indexView.html')
--- tpfinalaplicada/urls.py
+++ tpfinalaplicada/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pizzeria.views import mainView
 
 urlpatterns = [
+    path('', mainView),
     path('admin/', admin.site.urls),
 ]
