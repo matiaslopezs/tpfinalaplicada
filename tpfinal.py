bd643de2efec8806680cc4dac950555c03fbcccc
matiaslopezs <matigls2@gmail.com>
2020-10-09 15:31:07 -0300

    añado 3 prints

--- tpfinal.py
+++ tpfinal.py
@@ -30,29 +30,7 @@ def func_ops(x, subconjunto, datos):
     return (x - temps[0]) / (mid - temps[0]) if temps[0] <= x < mid else (temps[1] - x) / (temps[1] - mid) if mid <= x < temps[1] else 0
 
 
-func_ops(180, DATOS.TEMPERATURA.BAJA, DATOS.TEMPERATURA.VALORES)
-func_ops(8.5, DATOS.TIEMPO.POCO, DATOS.TIEMPO.VALORES)
+print(func_ops(180, DATOS.TEMPERATURA.BAJA, DATOS.TEMPERATURA.VALORES))
+print(func_ops(8.5, DATOS.TIEMPO.POCO, DATOS.TIEMPO.VALORES))
+print(func_ops(2, DATOS.NIVEL_PIZZA.POCO_COCINADA, DATOS.NIVEL_PIZZA.VALORES))
 
-
-"""
-def nivel_pizza(x, subconjunto):
-    if subconjunto == 'poco cocinada':
-        if 0 <= x < 2:
-            return (x - 0) / (2 - 0)
-        elif 2 <= x < 4:
-            return (4 - x) / (4 - 2)
-    elif subconjunto == 'en su punto':
-        if 3 <= x < 5:
-            return (x - 3) / (5 - 3)
-        elif 5 <= x < 7:
-            return (7 - x) / (7 - 5)
-    elif subconjunto == 'muy cocinada':
-        if 6 <= x < 8:
-            return (x - 6) / (8 - 6)
-        elif 8 <= x < 10:
-            return (10 - x) / (10 - 8)
-    return 0
-"""
-
-
-#print(tiempo(8.5, 'mucho'))
